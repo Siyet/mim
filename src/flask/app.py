33c4504ff1f6ef8ddd20704265674a454cfbc8f1
Denis <denp00@yandex.ru>
2019-07-27 19:14:49 +0300

    Update app.py

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,14 +1,9 @@
-
 from flask import Flask
 from flask import request
 from flask import abort
 import requests
 import datetime
 import json
-#import MySQLdb
-#import mysql.connector
-#from secret import passSQL
-#from flask import jsonify
 
 app = Flask(__name__)
 
@@ -30,6 +25,15 @@ def apipage():
         status = request.args.get('s')
 	error = request.args.get('e', default='null')
 	milliseconds = 0
+
+	url = 'http://95.214.63.201:5984/lifts/'+str(uid)
+	r = requests.get(url, headers=header)
+	if 'error' in r.text:
+		return 'authentication failed'
+	js = json.loads(r.text)
+	parentid = js['_id']
+	parentrev = js['_rev']
+
         if len(time) == 13:
             milliseconds = int(time[-3:])
         time = float(time[0:-3])
@@ -37,30 +41,27 @@ def apipage():
         the_date = datetime.datetime.fromtimestamp(time)
         the_date += datetime.timedelta(milliseconds=milliseconds)
  	
-	dat = {'lid':uid,'date':(str(the_date).replace(' ','T')+'Z'),'status':status, 'error':error}
+	status = str(status)
+	floor = 1+int(status[0:5], 2)
+	doors = status[5]
+	if doors == '1':
+  	  doors = 'false'
+	else:
+ 	   doors = 'true'
+
+	passenger = status[6]
+	if passenger == '1':
+	    passenger = 'false'
+	else:
+	    passenger = 'true'
+
+	dat = {'parent':{'_id':parentid,'_rev':parentrev},'date':(str(the_date).replace(' ','T')+'Z'),'floor':floor,'is_open':doors,'is_empy':passenger, 'error':error}
 	url = 'http://95.214.63.201:5984/status'
 	r = requests.post(url, headers=header, json=dat)
 	return r.text
 
-        #return str(uid)+str(the_date)+str(status)
     except Exception as e:
         return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
-'''
-@app.route('/api/err/', methods=['GET'])
-def errorsget():
-    try:
-        cursor.execute("SELECT * FROM errors")
-        row = str(cursor.fetchall())
-        t = row.split('), (')
-        t[0] = t[0].replace('((','')
-        t[-1] = t[-1].replace('))','')
-        row = ''
-        for el in t:
-            row = row + str(el)+'\n'
-        return str(row)
-    except Exception as e:
-        return '500 '+str(e)
-'''
